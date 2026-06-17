f077605f03366888048c80a14a265078edca6947
sbmatheuss <sbmatheus20@gmail.com>
2026-06-16 23:37:21 -0300

    reload

--- data/db_config.js
+++ data/db_config.js
@@ -1,8 +1,14 @@
 const knex = require('knex')
 const config = require("../knexfile.js")
 
-const env = process.env.DATABASE_URL ? 'production' : 'development'
+const isVercel = !!process.env.VERCEL
+const hasDB = !!process.env.DATABASE_URL
+
+if (isVercel && !hasDB) {
+  console.error('ERRO: DATABASE_URL não configurada. Adicione um banco PostgreSQL nas variáveis de ambiente da Vercel.')
+}
+
+const env = hasDB ? 'production' : 'development'
 const dbknex = knex(config[env])
 
 module.exports = dbknex
-
--- knexfile.js
+++ knexfile.js
@@ -46,6 +46,7 @@ module.exports = {
       max: 10
     },
     migrations: {
+      directory: './migrations',
       tableName: 'knex_migrations'
     }
   }
